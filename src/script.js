e2ff294f3b6b0b2c8fb57b1d876311734ee65b9c
Ivan Requena Garcia <ivanreq97@gmail.com>
2023-08-16 13:05:04 +0200

    label conflict solved

--- src/script.js
+++ src/script.js
@@ -12,8 +12,9 @@ const mapStates = require('./mapStateValues'); //Dataset with state values to be
 const RESULT_FILE_NAME = "Address.settings-meta-fixed.xml"; //Result file with xml name
 const LOG_FILE_NAME = "logs.txt"; //Result file with logs name
 
-//LOGS
-const logs = ['************** START SCRIPT ****************'];
+//CONSTANTS
+const lstSkipCountries = ['ES','AD']; //List of input countries that we want to bypass update despite it's in dataset
+const logs = ['************** START SCRIPT ****************']; //Logs array
 
 try 
 {
@@ -21,28 +22,34 @@ try
     const data = fs.readFileSync(METADATA_FILE_NAME, 'utf8');
     logs.push('Data successfully obtained from '+METADATA_FILE_NAME);
 
+    //Transform the input file and generate the output files
     parseString(data, function (err, result) {
+        //Auxiliar variables
+        var numStatesInp = 0; //Number of states to process from input metadata
+        var countryCount = 0; //Number of countries updated
+        var stateCount = 0; //Number of states updated
+        var countryNotProcessed = []; //List with IsoCode of input countries not in the dataset
+        var stateNotProcessed = []; //List with IsoCode of input states not in the dataset
+
+        //Store countries and states list from the input metadata in root variable
         var root = result.AddressSettings.countriesAndStates[0].countries;
+
+        //Add logs with numbers of countries and states in the input metadata
         logs.push('Countries to process: '+root.length);
-        var numStates = 0;
         for(var i = 0; i < root.length; i++)
         {
             if(root[i].states != null){
                 for(var j = 0; j < root[i].states.length; j++) {
-                    numStates = numStates + root[i].states.length;
+                    numStatesInp = numStatesInp + root[i].states.length;
                 }
             }
         }
-        logs.push('States to process: '+numStates);
+        logs.push('States to process: '+numStatesInp);
 
-        var countryNotProcessed = [];
-        var stateNotProcessed = [];
-        var countryCount = 0;
-        var stateCount = 0;
-
-
+        //Iterate over the countries and states list
         for(var i = 0; i < root.length; i++)
         {
+            //Country item data variables
             var countryCode = root[i].isoCode[0];
             var countryLbl = root[i].label[0];
             var countryIntVal = root[i].integrationValue[0];
@@ -50,42 +57,55 @@ try
             var countryVisible = root[i].visible[0];
             var countryStandard = root[i].standard[0];
 
-            //If any country doesn't has to be modified, his IsoCode must be included in this if condition
-            //In this case, Andorra and Spain are skipped, despite values are included in the dataset for update
-            if(countryCode != 'AD' && countryCode != 'ES') {
+            //Check if any country must to be skipped
+            if(!lstSkipCountries.includes(countryCode)) {
+
+                //Add log with country item information
                 logs.push('*****************************');
                 logs.push('***** NEW COUNTRY INPUT *****');
-                logs.push('Country input -> {label: '+countryLbl+', code: '+countryCode+', intVal: '+countryIntVal+', active: '+countryActive+', visible: '+countryVisible+', standard: '+countryStandard+'}');
+                logs.push('Country input -> '+
+                    '{label: '+countryLbl+
+                    ', code: '+countryCode+
+                    ', intVal: '+countryIntVal+
+                    ', active: '+countryActive+
+                    ', visible: '+countryVisible+
+                    ', standard: '+countryStandard+
+                '}');
 
+                //Check if the country isoCode is in the countries dataset and update values
                 if(mapCountry.has(countryCode)){
                     root[i].active[0] = true;
                     root[i].visible[0] = true;
+                    root[i].integrationValue[0] = mapCountry.get(countryCode);
                     root[i].label[0] = mapCountry.get(countryCode);
-                    root[i].integrationValue[0] = countryCode;
-                    logs.push('Country modified -> {label: '+mapCountry.get(countryCode)+', code: '+countryCode+', intVal: '+countryCode+', active: true, visible: true, standard: '+countryStandard+'}');
+                    logs.push('Country '+countryCode+' modified -> {label: '+mapCountry.get(countryCode)+', code: '+countryCode+', intVal: '+mapCountry.get(countryCode)+', active: true, visible: true, standard: '+countryStandard+'}');
                     mapCountry.delete(countryCode);
                     countryCount++;
-                } else {
+                } else { //If it's not in the dataset, visible is set to false and IsoCode is added to the list for the log
                     root[i].visible[0] = false;
                     countryNotProcessed.push(countryCode);
+                    logs.push('404: Country '+countryLbl+'('+countryCode+') not found in the dataset. Visibility set to false. Standard = '+countryStandard);
                 }
 
+                //Add log with num of states to process for the country item
                 if(root[i].states != null) logs.push('# STATES INPUTS FOR THIS COUNTRY: '+root[i].states.length);
                 else logs.push('NO STATES TO PROCESS');
 
+                //Iterate over the input country states
                 if(root[i].states != null){
                     for(var j = 0; j < root[i].states.length; j++)
                     {
+                        //State item data variables
                         var state = root[i].states[j];
-                        
-                        var stateCode = state.isoCode[0];
+                        var stateCode = state.isoCode[0]; //STATE ISO CODE
+                        var stateKey = countryCode+'-'+stateCode; //DATASET KEY
                         var stateLbl = state.label[0];
                         var stateIntVal = state.integrationValue[0];
                         var stateActive = state.active[0];
                         var stateVisible = state.visible[0];
                         var stateStandard = state.standard[0];
-                        var stateKey = countryCode+'-'+stateCode;
-                                  
+                            
+                        //Add log with state input info
                         logs.push('State input -> '+
                             '{key: '+stateKey+
                             ', label: '+stateLbl+
@@ -96,37 +116,39 @@ try
                             ', standard: '+stateStandard+
                         '}');
 
+                        //Check if the state isoCode is in the states dataset and update values
                         if(mapStates.has(stateKey)){
-                            root[i].states[j].integrationValue[0] = stateCode;
+                            root[i].states[j].label[0] = mapStates.get(stateKey);
+                            root[i].states[j].integrationValue[0] = mapStates.get(stateKey);
                             root[i].states[j].active[0] = true;
                             root[i].states[j].visible[0] = true;
-                            root[i].states[j].label[0] = mapStates.get(stateKey);
                             mapStates.delete(stateKey);
                             stateCount++;
                             
+                            //Add log with state processed
                             logs.push('State  '+stateKey+' modified -> '+
-                                '{label: '+stateLbl+
+                                '{label: '+mapStates.get(stateKey)+
                                 ', code: '+stateCode+
-                                ', intVal: '+stateIntVal+
-                                ', active: '+stateActive+
-                                ', visible: '+stateVisible+
+                                ', intVal: '+mapStates.get(stateKey)+
+                                ', active: true'+
+                                ', visible: true'+
                                 ', standard: '+stateStandard+
                             '}');
-                            
-                        } else { // State in org not in dataset. VISIBLE = FALSE
+                        
+                        } else { //If it's not in the dataset, visible is set to false and IsoCode is added to the list for the log
                             root[i].states[j].visible[0] = false;
                             stateNotProcessed.push(stateKey);
-
-                            //Add log
-                            logs.push('Input state '+stateLbl+'('+stateKey+') not found in the dataset. Visibility set to false. Standard = '+stateStandard);
+                            logs.push('404: State '+stateLbl+'('+stateKey+') not found in the dataset. Visibility set to false. Standard = '+stateStandard);
                         }
                     }
                 }
             } else {
-                logs.push('SKIPPED COUNTRY '+countryLbl+ '. INPUT -> '+root[i]);
+                countryNotProcessed.push(countryCode);
+                logs.push('SKIPPED COUNTRY '+countryLbl);
             }
         }
         
+        //Generate output file
         var builder = new xml2js.Builder();
         var xml = builder.buildObject(result);
     
@@ -135,8 +157,8 @@ try
                 return console.log(err);
             }
 
-            // DISPLAY FINAL STATUS
-            console.log("The file was generated and saved!");
+            // DISPLAY FINAL STATUS IN CONSOLE
+            console.log("The file was generated and saved! You can use the \'"+RESULT_FILE_NAME+"\' to deploy your metadata into SF");
             console.log('************** SUMMARY ****************');
             console.log('- Processed countries: '+countryCount);
             console.log('- Processed states: '+stateCount);
@@ -161,21 +183,28 @@ try
             }
             logs.push('***************************************');
 
+            //GENERATE LOGS FILE
             writeLogs();
         });     
     });
     
 } catch (err) 
 {
+    //Display the error and store it in logs
     console.error(err);
     logs.push('ERROR: '+err);
     writeLogs();
 }
 
+//Helper function that generate logs output file
 function writeLogs() {
-    fs.writeFileSync(LOG_FILE_NAME, '');
-    logs.forEach(message => {
-        fs.appendFileSync(LOG_FILE_NAME, message + '\n');
-    });
-    console.log('See the \''+LOG_FILE_NAME+'\' for more information');
+    try {
+        fs.writeFileSync(LOG_FILE_NAME, '');
+        logs.forEach(message => {
+            fs.appendFileSync(LOG_FILE_NAME, message + '\n');
+        });
+        console.log('See the \''+LOG_FILE_NAME+'\' for more information');
+    } catch (error) {
+        console.log('Error generating logs: '+error);
+    }
 }
